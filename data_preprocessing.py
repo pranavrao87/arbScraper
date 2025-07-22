bb7ec4d441975ac94023a9c901947b2c0e0aab5f
Pranav Rao <pranav.rao@utexas.edu>
2025-07-21 20:26:00 -0500

    began training model and predicting on previous year data

---
+++ data_preprocessing.py
@@ -0,0 +1,160 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns',1000)
+pd.set_option('display.max_rows',1000)
+
+# Column names for data
+colNames = ['date','dblheader_code','day_of_week','team_v','league_v','game_no_v',
+           'team_h','league_h','game_no_h', 'runs_v', 'runs_h','outs_total','day_night',
+            'completion_info','forfeit_info','protest_info','ballpark_id','attendance','game_minutes',
+            'linescore_v','linescore_h',
+           'AB_v','H_v','2B_v','3B_v','HR_v','RBI_v','SH_v','SF_v','HBP_v','BB_v','IBB_v','SO_v',
+            'SB_v', 'CS_v','GIDP_v','CI_v','LOB_v',
+            'P_num_v','ERind_v','ERteam_v','WP_v','balk_v',
+            'PO_v','ASST_v','ERR_v','PB_v','DP_v','TP_v',
+           'AB_h', 'H_h', '2B_h', '3B_h', 'HR_h', 'RBI_h', 'SH_h', 'SF_h', 'HBP_h', 'BB_h', 'IBB_h','SO_h',
+            'SB_h', 'CS_h', 'GIDP_h', 'CI_h', 'LOB_h',
+            'P_num_h', 'ERind_h', 'ERteam_h', 'WP_h', 'balk_h',
+            'PO_h', 'ASST_h', 'ERR_h', 'PB_h', 'DP_h', 'TP_h',
+            'ump_HB_id', 'ump_HB_name','ump_1B_id', 'ump_1B_name','ump_2B_id', 'ump_2B_name',
+            'ump_3B_id', 'ump_3B_name','ump_LF_id', 'ump_LF_name','ump_RF_id', 'ump_RF_name',
+            'mgr_id_v', 'mgr_name_v', 'mgr_id_h', 'mgr_name_h',
+            'pitcher_id_w','pitcher_name_w','pitcher_id_l','pitcher_name_l','pitcher_id_s','pitcher_name_s',
+            'GWRBI_id','GWRBI_name','pitcher_start_id_v','pitcher_start_name_v','pitcher_start_id_h','pitcher_start_name_h',
+            'batter1_name_v', 'batter1_id_v', 'batter1_pos_v', 'batter2_name_v', 'batter2_id_v', 'batter2_pos_v',
+            'batter3_name_v', 'batter3_id_v', 'batter3_pos_v', 'batter4_name_v', 'batter4_id_v', 'batter4_pos_v',
+            'batter5_name_v', 'batter5_id_v', 'batter5_pos_v', 'batter6_name_v', 'batter6_id_v', 'batter6_pos_v',
+            'batter7_name_v', 'batter7_id_v', 'batter7_pos_v', 'batter8_name_v', 'batter8_id_v', 'batter8_pos_v',
+            'batter9_name_v', 'batter9_id_v', 'batter9_pos_v', 'batter1_name_h', 'batter1_id_h', 'batter1_pos_h',
+            'batter2_name_h', 'batter2_id_h', 'batter2_pos_h', 'batter3_name_h', 'batter3_id_h', 'batter3_pos_h',
+            'batter4_name_h', 'batter4_id_h', 'batter4_pos_h', 'batter5_name_h', 'batter5_id_h', 'batter5_pos_h',
+            'batter6_name_h', 'batter6_id_h', 'batter6_pos_h', 'batter7_name_h', 'batter7_id_h', 'batter7_pos_h',
+            'batter8_name_h', 'batter8_id_h', 'batter8_pos_h', 'batter9_name_h', 'batter9_id_h', 'batter9_pos_h',           
+           'misc_info','acqui_info'
+           ]
+
+
+df = pd.DataFrame()
+for year in range(2010, 2025):
+    file = "/Users/Prana/Documents/arbScraper/gl/gl" + str(year) + ".txt"
+    df_temp = pd.read_csv(file, header=None)
+    df_temp.columns = colNames
+    df_temp['season'] = year
+    df = pd.concat((df, df_temp))
+
+# drop columns that don't matter
+cols_to_drop = ['ballpark_id', 'attendance', 'ump_HB_id', 'ump_HB_name','ump_1B_id', 'ump_1B_name','ump_2B_id', 'ump_2B_name',
+            'ump_3B_id', 'ump_3B_name','ump_LF_id', 'ump_LF_name','ump_RF_id', 'ump_RF_name',
+            'mgr_id_v', 'mgr_name_v', 'mgr_id_h', 'mgr_name_h', 'batter5_name_h', 'batter5_id_h', 'batter5_pos_h',
+            'batter6_name_h', 'batter6_id_h', 'batter6_pos_h', 'batter7_name_h', 'batter7_id_h', 'batter7_pos_h',
+            'batter8_name_h', 'batter8_id_h', 'batter8_pos_h', 'batter9_name_h', 'batter9_id_h', 'batter9_pos_h', 
+            'batter5_name_v', 'batter5_id_v', 'batter5_pos_v', 'batter6_name_v', 'batter6_id_v', 'batter6_pos_v',
+            'batter7_name_v', 'batter7_id_v', 'batter7_pos_v', 'batter8_name_v', 'batter8_id_v', 'batter8_pos_v',
+            'batter9_name_v', 'batter9_id_v', 'batter9_pos_v', ]
+df.drop(columns=cols_to_drop, inplace=True)
+
+df['run_diff'] = df['runs_h']-df['runs_v']
+df['home_victory'] = (df['run_diff']>0).astype(int)
+df['run_total'] = df['runs_h'].copy()+df['runs_v'].copy()
+df['date_dblhead'] = (df['date'].astype(str) + df['dblheader_code'].astype(str)).astype(int)
+
+def strip_suffix(x, suff):
+    if x.endswith(suff):
+        return(x[:-len(suff)])
+    else:
+        return(x)
+
+visit_cols = [col for col in df.columns if not col.endswith('_h')]
+visit_cols_stripped = [strip_suffix(col,'_v') for col in visit_cols]
+home_cols = [col for col in df.columns if not col.endswith('_v')]
+home_cols_stripped = [strip_suffix(col,'_h') for col in home_cols]
+
+
+def create_team_df(team):
+    """
+    Subsets the game level df by team in order to aggregate team statistics easily
+    Also creates rolling sums for important statistics to represent the stats of a team up to the game, 
+    but not including the game in question to set up training data for model to predict on
+    """
+
+    # all the away games of a team
+    df_team_v = df[(df.team_v==team)]
+    opponent = df_team_v['team_h']
+    df_team_v = df_team_v[visit_cols]
+    df_team_v.columns = visit_cols_stripped
+    df_team_v['home_game'] = 0
+    df_team_v['opponent'] = opponent
+
+    # all the home games of a team
+    df_team_h = df[(df.team_h==team)]
+    opponent = df_team_h['team_v']
+    df_team_h = df_team_h[home_cols]
+    df_team_h.columns = home_cols_stripped
+    df_team_h['home_game'] = 1
+    df_team_h['opponent'] = opponent
+
+
+    df_team = pd.concat((df_team_h, df_team_v))
+    df_team.sort_values(['date', 'game_no'],inplace=True)
+    
+    for winsize in [162,30]: #162 games = 1 season worth of games
+        suff = str(winsize)
+        for raw_col in ['AB','H','2B','3B','HR','BB','runs','SB','CS','ERR']:
+            new_col = 'rollsum_'+raw_col+'_'+suff
+            df_team[new_col] = df_team[raw_col].rolling(winsize, closed='left').sum()
+
+        # batting average = hits / at bat
+        df_team['rollsum_BATAVG_'+suff] = df_team['rollsum_H_'+suff] / df_team['rollsum_AB_'+suff]
+
+        # on base percentage = (hits + walks (base on balls)) / (at bats + walks) 
+        df_team['rollsum_OBP_'+suff] = (df_team['rollsum_H_'+suff] + df_team['rollsum_BB_'+suff]) / (df_team['rollsum_AB_'+suff]+df_team['rollsum_BB_'+suff])
+
+        # slugging percentage: total bases a player earns per at-bat
+        # (hits + second base * 2 + third base * 3 + home runs * 4) / at bats
+        df_team['rollsum_SLG_'+suff] = (df_team['rollsum_H_'+suff] + df_team['rollsum_2B_'+suff] * 2 + df_team['rollsum_3B_'+suff] * 3 + df_team['rollsum_HR_'+suff] * 4) / (df_team['rollsum_AB_'+suff])
+
+        # cumulative sum of slugging percentages
+        df_team['rollsum_OBS_'+suff] = df_team['rollsum_OBP_'+suff] + df_team['rollsum_SLG_'+suff]
+    
+    df_team['season_game'] = df_team['season']*1000 + df_team['game_no']
+    df_team.set_index('season_game', inplace=True)
+    return(df_team)
+
+
+team_data_dict = {}
+for team in df.team_v.unique():
+    team_data_dict[team] = create_team_df(team)
+
+# list of all stat names and time windows
+stats = ['BATAVG', 'OBP', 'SLG', 'OBS', 'SB', 'CS', 'ERR']
+windows = ['162', '30']
+sides = ['h', 'v'] 
+
+# init all arrays in a dictionary
+stat_arrays = {
+    f'{stat}_{window}_{side}': np.zeros(df.shape[0])
+    for stat in stats for window in windows for side in sides
+}
+
+# loop through each row and populate values
+for i, row in df.iterrows():
+    home_team = row['team_h']
+    visit_team = row['team_v']
+    game_index_h = row['season'] * 1000 + row['game_no_h']
+    game_index_v = row['season'] * 1000 + row['game_no_v']
+
+    for stat in stats:
+        for window in windows:
+            colname = f'rollsum_{stat}_{window}'
+            stat_arrays[f'{stat}_{window}_h'][i] = team_data_dict[home_team].loc[game_index_h, colname]
+            stat_arrays[f'{stat}_{window}_v'][i] = team_data_dict[visit_team].loc[game_index_v, colname]
+
+# add arrays back to dataframe
+for key, arr in stat_arrays.items():
+    df[key] = arr
+
+df = df[df.run_diff != 0] # drop tie games from dataset b/c doesn't count as victory or loss
+
+df.to_csv('df_bp1.csv', index=False)
--- model.py
+++ model.py
@@ -5,7 +5,8 @@ from pprint import pprint
 from io import StringIO
 
 
-# Extract team standings data for 2025 season (the table is wrapped inside a comment)
+
+# team standings data for 2025 season (the table is wrapped inside a comment)
 standings_url = "https://www.baseball-reference.com/leagues/MLB-standings.shtml"
 response1 = requests.get(standings_url)
 soup1 = BeautifulSoup(response1.text, "html.parser")
@@ -22,7 +23,7 @@ for comment1 in comments1:
             print(df_standings.head())
             break
 
-# Extract team batting stats for 2025 season
+# team batting stats for 2025 season
 stats_url = "https://www.baseball-reference.com/leagues/majors/2025.shtml"
 response2 = requests.get(stats_url)
 soup2 = BeautifulSoup(response2.text, "html.parser")
@@ -32,14 +33,14 @@ df_batting = pd.read_html(StringIO(str(table_batting)))[0]
 print("=== Team Batting Stats ===")
 print(df_batting.head())
 
-# Extract team pitching stats for 2025 season (this table is nested in a comment)
+# team pitching stats for 2025 season (this table is nested in a comment)
 comments2 = soup2.find_all(string=lambda text: isinstance(text, Comment))
 for comment in comments2:
     if 'teams_standard_pitching' in comment:
         commented_soup2 = BeautifulSoup(comment, "html.parser")
         table_pitching = commented_soup2.find("table", {"id": "teams_standard_pitching"})
         if table_pitching:
-            df_pitching = pd.read_html(StringIO(str(table)))[0]
+            df_pitching = pd.read_html(StringIO(str(table_pitching)))[0]
             print("=== Team Pitching Stats ===")
             print(df_pitching.head())
             break
@@ -52,5 +53,23 @@ What stats do I want to use to predict outcomes?
     - Home/away performance
     - R/G and RA/G --> runs / game and runs allowed / game
 """
+
+# Get a dataframe of odds just for the current matchups/games to predict on
+df_espn = pd.read_csv('espn_odds.csv')
+
+
+for _, game in df_espn.iterrows():
+    homeTeam = game["TEAM_HOME"]
+    awayTeam = game["TEAM_AWAY"]
+
+    # Get the matching rows for the home and away teams from standings, pitching, and batting tables
+    standings_home_row = df_standings[(df_standings["Tm"] == homeTeam)]
+    standings_away_row = df_standings[(df_standings["Tm"] == awayTeam)]
+
+    batting_home_row = df_batting[(df_standings["Tm"] == homeTeam)]
+    batting_away_row = df_batting[(df_standings["Tm"] == awayTeam)]
+
+    pitching_home_row = df_pitching[(df_standings["Tm"] == homeTeam)]
+    pitching_away_row = df_pitching[(df_standings["Tm"] == awayTeam)]
 
 
